7faf2a53330abe48a49f07fa4c09ec98281eed79
MaximeBaudin <baudin.maximepro@gmail.com>
2022-09-21 09:48:01 +0200

    création du serpent 2/ avance

--- script.js
+++ script.js
@@ -20,27 +20,49 @@ window.onload = function(){
         document.body.appendChild(canvas); //permet d'attacher l'element à notre page HTML
     
         ctx = canvas.getContext("2d");
+        snakee = new Snake([[6,4], [5,4], [4,4]]);
         refreshCanvas();
         
     }
 
     function refreshCanvas(){
-        xCord += 3;
-        yCord += 3;
+
         ctx.clearRect(0,0, canvas.width, canvas.height);
         ctx.beginPath(); //ne pas oublier cette ligne
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect(xCord,yCord,100,50);
+        snakee.advance();
+        snakee.draw();
         setTimeout(refreshCanvas, delay); //appel de la fonction toutes les 1000 ms
     }
+
+    function drawBlock(ctx, position){
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+
+        ctx.fillRect(x,y,blockSize, blockSize);
+    }
+
+    
 
     function Snake(body){
 
         this.body = body;
+
         this.draw = function()
         {
             ctx.save();
             ctx.fillStyle = "#ff0000";
+            for(var i = 0; i < this.body.length; i++)
+            {
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        }
+
+        this.advance = function(){
+            var nextPosition = this.body[0].slice(); //copier l'element
+            nextPosition[0] += 1; //Avancer le X du serpent 
+            this.body.unshift(nextPosition); //Rajoute la nouvelle position à la première place
+            this.body.pop(); //supprime le dernier élement
         }
     }
     
